eaa3b1984c8abca59efc7a0aa4ac50cf89974b79
Hreaun <43428176+Hreaun@users.noreply.github.com>
2020-02-24 14:16:47 +0700

    Add files via upload

--- lab2/Main.java
+++ lab2/Main.java
@@ -7,9 +7,23 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.EmptyStackException;
 import java.util.Properties;
 import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 
 public class Main {
+    static Logger log;
+
+    static {
+        try (FileInputStream ins = new FileInputStream("C:\\Users\\troll\\IdeaProjects\\lab2\\src\\main\\lab2\\log.config")) {
+            LogManager.getLogManager().readConfiguration(ins);
+            log = Logger.getLogger(Main.class.getName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
     static public void main(String[] argv) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         Input in;
         if (argv.length == 0) {
@@ -19,12 +33,13 @@ public class Main {
             try {
                 in = new ByFile(argv[0]);
             } catch (FileNotFoundException ex) {
+                log.logp(Level.SEVERE, "Main", "main", ex.getLocalizedMessage());
                 return;
             }
         }
 
         Properties commands = new Properties();
-        commands.load(new FileInputStream("src/main/java/Commands/commands.properties"));
+        commands.load(new FileInputStream("C:\\Users\\troll\\IdeaProjects\\lab2\\src\\main\\lab2\\Commands\\commands.properties"));
         Calculator calc = new Calculator();
 
         while (true) {
@@ -35,16 +50,16 @@ public class Main {
                 Object command = Class.forName(commands.getProperty(str[0])).getDeclaredConstructor().newInstance();
                 Factory.registerCommand(str[0], (Supplier<? extends Commands>) command);
             } catch (NullPointerException ex) {
-                System.out.println("Wrong command.");
+                log.logp(Level.WARNING, "Main", "main", "Wrong command: " + str[0]);
                 continue;
             }
             calc.setCommand(Factory.getCommand(str[0]));
             try {
                 calc.exeCommand(str);
             } catch (EmptyStackException ex) {
-                System.out.println("There is nothing on stack.");
+                log.logp(Level.WARNING, "Main", "main", "There is nothing on stack.");
             } catch (Exception ex) {
-                System.out.println(ex.getMessage());
+                log.log(Level.WARNING, ex.getMessage());
             }
 
         }
